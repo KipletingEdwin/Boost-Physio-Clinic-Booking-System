9e2d48444148e8c07f36a82f8f80ee61f38c323a
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-08 17:37:03 +0000

    Add addTreatmnet method

--- src/com/boostphysio/model/Physiotherapist.java
+++ src/com/boostphysio/model/Physiotherapist.java
@@ -1,5 +1,6 @@
 package com.boostphysio.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Physiotherapist {
@@ -9,4 +10,16 @@ public class Physiotherapist {
     private  String contact;
     private List<String> expertise;
     private List<Treatment> treatments;
+
+    public Physiotherapist(int id, String name, String contact, List<String> expertise){
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        this.expertise = new ArrayList<>(expertise);
+        this.treatments = new ArrayList<>();
+    }
+
+    public  void  addTreatment(Treatment treatment){
+        treatments.add(treatment);
+    }
 }
