1329e96548e418edaf610be26d295c185b0c3205
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-08 17:31:24 +0000

    Declare variables in Physiotherapist class

--- src/com/boostphysio/model/Physiotherapist.java
+++ src/com/boostphysio/model/Physiotherapist.java
@@ -1,4 +1,12 @@
 package com.boostphysio.model;
 
+import java.util.List;
+
 public class Physiotherapist {
+
+    private  int id;
+    private  String name;
+    private  String contact;
+    private List<String> expertise;
+    private List<Treatment> treatments;
 }
