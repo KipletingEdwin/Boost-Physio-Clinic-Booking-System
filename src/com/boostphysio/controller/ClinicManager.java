0e6bd6c0f1294087c564e25d2237bd1297da35b0
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-09 09:00:20 +0000

    Add four variables to ClinicManager class

--- src/com/boostphysio/controller/ClinicManager.java
+++ src/com/boostphysio/controller/ClinicManager.java
@@ -1,4 +1,15 @@
 package com.boostphysio.controller;
 
+import com.boostphysio.model.Appointment;
+import com.boostphysio.model.Patient;
+import com.boostphysio.model.Physiotherapist;
+
+import java.util.List;
+
 public class ClinicManager {
+    private List<Physiotherapist> physiotherapists;
+    private  List<Patient> patients;
+    private  List<Appointment> appointments;
+    private int bookingIdCounter;
+
 }
