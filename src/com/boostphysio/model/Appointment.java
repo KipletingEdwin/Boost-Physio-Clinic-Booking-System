04e18903d74f91d4ac14cceafb27fa07611b2e97
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-09 08:50:37 +0000

    Add four variables to appointment class

--- src/com/boostphysio/model/Appointment.java
+++ src/com/boostphysio/model/Appointment.java
@@ -1,4 +1,9 @@
 package com.boostphysio.model;
 
 public class Appointment {
+    private  int bookingId;
+    private  Patient patient;
+    private  Treatment treatment;
+    private  Physiotherapist physiotherapist;
+    private  String status;
 }
