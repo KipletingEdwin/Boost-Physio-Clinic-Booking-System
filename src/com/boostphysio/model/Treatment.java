78a55a93e4d6c97af67edb82e21d95916714aaf3
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-09 08:46:42 +0000

    Add three variables to treatment class

--- src/com/boostphysio/model/Treatment.java
+++ src/com/boostphysio/model/Treatment.java
@@ -1,4 +1,8 @@
 package com.boostphysio.model;
 
 public class Treatment {
+    private  String name;
+    private  String date;
+    private  String time;
+    private  Physiotherapist physiotherapist;
 }
