009d67d6f7f600f8475e8f2581ea373d2c974b3a
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-09 08:44:35 +0000

    Define the constructor

--- src/com/boostphysio/model/Patient.java
+++ src/com/boostphysio/model/Patient.java
@@ -5,4 +5,10 @@ public class Patient {
     private  int id;
     private  String name;
     private  String contact;
+
+    public  Patient(int id, String name, String contact){
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+    }
 }
