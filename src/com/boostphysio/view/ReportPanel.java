60425a03bb5e5412fe3be6620091f1386bac44c7
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-10 09:44:14 +0000

    Add report display area

--- src/com/boostphysio/view/ReportPanel.java
+++ src/com/boostphysio/view/ReportPanel.java
@@ -1,4 +1,17 @@
 package com.boostphysio.view;
 
-public class ReportPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ReportPanel extends JPanel {
+    public ReportPanel() {
+        //Add report panel components here
+        setLayout(new BorderLayout());
+
+        //Report Display Area
+
+        JTextArea reportArea = new JTextArea();
+        reportArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(reportArea);
+    }
 }
