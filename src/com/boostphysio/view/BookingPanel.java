404bc68d0d95957995b6834daa6751a090287876
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-10 09:28:01 +0000

    Add buttons to book,cancel and attend appointments

--- src/com/boostphysio/view/BookingPanel.java
+++ src/com/boostphysio/view/BookingPanel.java
@@ -27,5 +27,22 @@ public class BookingPanel extends JPanel {
         JScrollPane scrollPane = new JScrollPane(appointmentList);
 
         //Buttons for Booking,Canceling, and Attending Appointments
+        JPanel buttonPanel = new JPanel();
+        JButton bookButton = new JButton("Book Appointment");
+        JButton cancelButton = new JButton("Cancel Appointment");
+        JButton attendButton = new JButton("Attend Appointment");
+
+        buttonPanel.add(bookButton);
+        buttonPanel.add(cancelButton);
+        buttonPanel.add(attendButton);
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.add(bookButton);
+        bottomPanel.add(cancelButton);
+        bottomPanel.add(attendButton);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 }
