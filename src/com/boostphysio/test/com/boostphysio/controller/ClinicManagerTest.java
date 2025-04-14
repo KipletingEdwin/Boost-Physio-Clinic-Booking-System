cf282757f9d0611ef3f2228b06379e125baf2c68
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-04-14 18:03:39 +0100

    Add test file for Clinic Manager

---
+++ src/com/boostphysio/test/com/boostphysio/controller/ClinicManagerTest.java
@@ -0,0 +1,62 @@
+package com.boostphysio.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClinicManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPhysiotherapists() {
+    }
+
+    @Test
+    void getAppointments() {
+    }
+
+    @Test
+    void getPatients() {
+    }
+
+    @Test
+    void addPatient() {
+    }
+
+    @Test
+    void addPhysiotherapist() {
+    }
+
+    @Test
+    void removePatient() {
+    }
+
+    @Test
+    void bookAppointment() {
+    }
+
+    @Test
+    void cancelAppointment() {
+    }
+
+    @Test
+    void attendAppointment() {
+    }
+
+    @Test
+    void changeAppointment() {
+    }
+
+    @Test
+    void generateReport() {
+    }
+}
---
+++ src/com/boostphysio/test/com/boostphysio/model/PatientTest.java
@@ -0,0 +1,38 @@
+package com.boostphysio.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void getContact() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
